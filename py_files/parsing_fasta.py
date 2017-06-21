6c71b2d4201b07ef0d2ee0545187ca5a24742243
armanmansourian <mansourianarman@gmail.com>
2017-06-21 09:50:10 -0700

    deleted parsing_fasta.py file

--- py_files/parsing_fasta.py
+++
@@ -1,8 +0,0 @@
-import os
-
-"""
-Using 'with' block to read sequence and store as single string with no gaps.
-"""
-
-with open('~git/bootcamp/data/salmonella_spi1_region.fna') as f:
-    f_str = f.readlines[1:20]
